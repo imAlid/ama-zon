48a8340ff29a55c3e5824613ee62c738d47f1abf
ImAlid <you@example.com>
2023-03-29 15:57:24 +0600

    added products info

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Product.css";
+const Product = (props) => {
+  console.log(props.product);
+  const { img, name, price, quantity, ratings, seller } = props.product;
+  return (
+    <div className="product">
+      <img src={img} alt="" />
+      <div className="product-info">
+        <h6 className="product-name">{name}</h6>
+        <p className="price">Price${price}</p>
+        <p className="manufacturer">Manufacturer{seller}</p>
+        <p className="rating">Rating: {ratings} Star</p>
+      </div>
+      <button className="cart-btn">Add to cart</button>
+    </div>
+  );
+};
+import "./Product.css";
+export default Product;
